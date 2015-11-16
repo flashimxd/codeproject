ee3b30454acc747379ac179fec5dc6bd32551ece
Rangel Netto <rangel@getrak.com.br>
2015-11-16 18:53:39 -0200

    corrigindo bugs

--- public/build/js/controllers/project-note/projectNoteEdit.js
+++ public/build/js/controllers/project-note/projectNoteEdit.js
@@ -1,8 +1,6 @@
 angular.module('app.controllers')
-    .controller('ClientEditController', ['$scope', 'Client', '$location','$routeParams', function($scope, Client, $location, $routeParams){
-        //console.log($routeParams.id); debugger;
+    .controller('ProjectNoteEditController', ['$scope', 'Client', '$location','$routeParams', function($scope, Client, $location, $routeParams){
         $scope.client = Client.get({id: $routeParams.id});
-        //console.log($scope.client); debugger;
         $scope.save = function(){
             if($scope.formCadastro.$valid){
 
